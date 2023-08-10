7027ef3113a6e9c5e1087c3fe645380ca6a997e5
Ashis Maity <amaity@digisprint.com>
2023-08-10 21:42:47 +0530

    string-literals

---
+++ Variable-DataType/strings/script.js
@@ -0,0 +1,18 @@
+//Template Literals
+
+const name = "John";
+x = `Hello my name is ${name}`
+console.log(x);
+
+//length
+console.log(name.length);
+
+//String properties and Methods
+const s = "Hello World!";
+x = typeof (s);
+console.log(x); //string
+
+const s1 = new String("Hello World!");
+x = typeof (s1);
+x = s1[0];
+console.log(x);//object
