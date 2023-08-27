1497988ec87a993fa6347f183a53fdf12cefb057
Ashis Maity <amaity@digisprint.com>
2023-08-27 22:36:39 +0530

    string methods

--- Variable-DataType/strings/script.js
+++ Variable-DataType/strings/script.js
@@ -13,6 +13,21 @@ x = typeof (s);
 console.log(x); //string
 
 const s1 = new String("Hello World!");
-x = typeof (s1);
+x = typeof (s1); //object
 x = s1[0];
-console.log(x);//object
+x = s1.charAt(0); // both are similar x = s1[0];
+const c = 'hello World bro!!!'
+x = c.toLowerCase();
+x = c.toUpperCase();
+x = c.indexOf('W'); // it will return index number.
+console.log(x, 'index');
+
+// find D@+ from this below string.
+const string = "#d@@cd76D2+kuL(ygP";
+const finding = string.indexOf('D@+'); // if we checked single single value , then we got index number.
+console.log(finding, 'finding');
+
+//substring
+const sub = 'Hello JavaScript';
+console.log(sub.substring(6)); // if substring take only one parameter that means it is postion of string and afterthat rest of the string.
+
